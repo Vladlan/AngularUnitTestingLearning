2ae31a1d430fa5407c6feaa31d1acf7b3b31d9d4
Vladlan <lanvlad911@gmail.com>
2018-04-12 20:43:06 +0300

    added simple test to message.service.ts

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,15 +1,37 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+// import { TestBed, inject } from '@angular/core/testing';
+//
 import { MessageService } from './message.service';
+//
+// describe('MessageService', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers: [MessageService]
+//     });
+//   });
+//
+//   it('should be created', inject([MessageService], (service: MessageService) => {
+//     expect(service).toBeTruthy();
+//   }));
+// });
 
+
+// Straight Jasmine testing without Angular's testing support
 describe('MessageService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MessageService]
-    });
+  let service: MessageService;
+  beforeEach(() => { service = new MessageService(); });
+
+  it('#should add to messages array "1" and "2" ', () => {
+    service.add('1');
+    service.add('2');
+    expect(service.messages[0]).toBe('1');
+    expect(service.messages[1]).toBe('2');
   });
 
-  it('should be created', inject([MessageService], (service: MessageService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('#method clear() should clear all messages', () => {
+    service.add('1');
+    service.add('2');
+    expect(service.messages[1]).toBe('2');
+    service.clear();
+    expect(service.messages.length).toBe(0);
+  });
 });
