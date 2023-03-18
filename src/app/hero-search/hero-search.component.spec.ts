b078c30afd72d5502ecacdffa154e9725c18199c
Vladlan <lanvlad911@gmail.com>
2023-03-18 18:26:27 +0100

    Added more tests

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,25 +1,30 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { HeroSearchComponent } from './hero-search.component';
-//
-// describe('HeroSearchComponent', () => {
-//   let component: HeroSearchComponent;
-//   let fixture: ComponentFixture<HeroSearchComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HeroSearchComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HeroSearchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
+
+import { HeroSearchComponent } from "./hero-search.component";
+
+describe("HeroSearchComponent", () => {
+  let component: HeroSearchComponent;
+  let fixture: ComponentFixture<HeroSearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [HeroSearchComponent],
+      providers: [HeroService, MessageService],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
