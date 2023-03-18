4de31717505987bf6100f9177894d3a231f6ce8b
Vladlan <lanvlad911@gmail.com>
2023-03-18 18:32:50 +0100

    Added more tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,25 +1,29 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { HeroesComponent } from './heroes.component';
-//
-// describe('HeroesComponent', () => {
-//   let component: HeroesComponent;
-//   let fixture: ComponentFixture<HeroesComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HeroesComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HeroesComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should be created', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HeroesComponent } from "./heroes.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
+
+describe("HeroesComponent", () => {
+  let component: HeroesComponent;
+  let fixture: ComponentFixture<HeroesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent],
+      providers: [HeroService, MessageService],
+      imports: [RouterTestingModule.withRoutes([]), HttpClientTestingModule],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should be created", () => {
+    expect(component).toBeTruthy();
+  });
+});
